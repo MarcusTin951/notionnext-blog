d368f7934c68fc3b45a6ca1f321bc5adfbc97c87
MarcusTin951 <marcustin69@gmail.com>
2025-04-13 08:46:24 +0800

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HQJ3NLZQVS"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HQJ3NLZQVS');
+</script>
