c9f668594224a5c4bc3530fdfb23a6bfb39ce7e3
MarcusTin951 <marcustin69@gmail.com>
2025-04-13 08:50:08 +0800

    Delete gtag.js

--- gtag.js
+++
@@ -1,9 +0,0 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-HQJ3NLZQVS"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-HQJ3NLZQVS');
-</script>
