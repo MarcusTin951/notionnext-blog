b56345dbcb2d6fd613d10e56bf17b128950a3c69
陈桂䶮 MARCUS TIN KWEE YEN <199439217+MarcusTin951@users.noreply.github.com>
2026-06-21 15:22:33 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '', url: '' },
-    { title: '', url: '' }
+ //   { title: '', url: '' }
   ],
 
   // 英雄区左右侧组件颠倒位置
